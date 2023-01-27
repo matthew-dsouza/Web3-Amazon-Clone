291e2039ba91bbd38ef4df8798c0546c46b8d9dc
Matthew Dsouza <matthewdsouza.one@gmail.com>
2023-01-28 00:59:23 +0530

    updated the deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,4 +1,4 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
+// I require the Hardhat Runtime Environment explicitly here. This is optional
 // but useful for running the script in a standalone fashion through `node <script>`.
 //
 // You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
@@ -41,7 +41,7 @@ async function main() {
   }
 }
 
-// We recommend this pattern to be able to use async/await everywhere
+// (Matthew) I recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main().catch((error) => {
   console.error(error);
